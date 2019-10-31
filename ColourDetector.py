db84a0a238e0f162e692ef2539e66cda8ab834c2
Meelis Pihlap <meelis.pihlap@estcube.eu>
2019-10-31 10:10:02 +0200

    Added use of np.full to clean up code

--- ColourDetector.py
+++ ColourDetector.py
@@ -21,13 +21,6 @@ class ColorFrequencyParser:
             "a#": (255, 236, 0),
             "b" : (153, 255, 0)
         })
-        # self.color_frequency_map = OrderedDict( \
-        # {
-        #     "red" : (255, 0, 0),
-        #     "green": (0, 255, 0),
-        #     "yellow": (255, 255, 0),
-        #     "blue" : (0, 0, 255)
-        # })
         
         self.lab = np.zeros((len(self.color_frequency_map), 1, 3), dtype="uint8")
         self.colorNames = []
@@ -41,17 +34,15 @@ class ColorFrequencyParser:
         cv2.drawContours(mask, [contour], -1, 255, -1)
         mask = cv2.erode(mask, None, iterations=2)
         mean = cv2.mean(image, mask=mask)[:3]
-        # print("mean: " + str(mean))
         minDist = (np.inf, None)
         for (i, row) in enumerate(self.lab):
             d = dist.euclidean(row[0], mean)
             if d < minDist[0]:
                 minDist = (d, i)
-        # print(minDist)
-        # print(self.lab[minDist[1]][0])
         if minDist[1] != None:
             return self.colorNames[minDist[1]]
         return 
+
     # Return BGR color value of color corresponding to note
     def getColor(self, note):
         return np.array(list(reversed(cfparser.color_frequency_map.get(note))), dtype=float)/255
@@ -59,36 +50,24 @@ class ColorFrequencyParser:
     def showPalette(self):
         increment = 0
         for key in self.color_frequency_map:
-            newimg = np.zeros((100, 100, 3))
-            for i in range(newimg.shape[0]):
-                for j in range(newimg.shape[1]):
-                    newimg[i][j] = np.array(list(reversed(cfparser.color_frequency_map.get(key))), dtype=float)/255
+            newimg = np.full((100, 100, 3), self.getColor(key))
             print(key, newimg[0][0])
             cv2.imshow(key, newimg)
             cv2.moveWindow(key, increment, 100)
             increment += 200
 
-
-
 filenames = ["solid_pink.png", "solid_grey.png", "solid_blue.png", "solid_brown.png"]
 cfparser = ColorFrequencyParser()
 
-# note to self - np.fill() vms
-newimg = np.zeros((100, 100, 3))
-for i in range(newimg.shape[0]):
-    for j in range(newimg.shape[1]):
-        #newimg[i][j] = [0, 0, 116]
-        newimg[i][j] = cfparser.getColor("f#")
+newimg = np.full((100, 100, 3), cfparser.getColor("f#"))
+
 print("f#", newimg[0][0])
 cv2.imshow("f#", newimg)    
 cv2.moveWindow("f#", 100, 100)
 
-newimg2 = np.zeros((100, 100, 3))
-for i in range(newimg2.shape[0]):
-    for j in range(newimg2.shape[1]):
-        newimg2[i][j] = cfparser.getColor("g#")
-print("g#", newimg2[0][0])
-cv2.imshow("g#", newimg2)
+newimg = np.full((100, 100, 3), cfparser.getColor("g#"))
+print("g#", newimg[0][0])
+cv2.imshow("g#", newimg)
 cv2.moveWindow("g#", 300, 100)
 cv2.waitKey(0)
 
@@ -116,10 +95,5 @@ for f in filenames:
     cv2.imshow("Closest match", newimg)
     cv2.moveWindow("Closest match", 0, 100)
     cv2.waitKey(0)
-
-
-
-
-
 
 cv2.destroyAllWindows()
